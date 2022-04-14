41501fcc1f8e77060d7734b02d35826e009f4398
malumo4a <103600946+malumo4a@users.noreply.github.com>
2022-04-15 00:26:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const generateImage = require("./generateImage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content === "!boyko"){
+        message.reply("Пари не ми трябват.Вече съм на такива години,че закъде да трупам пари.Стига ми къщата в Банкя!")
+    }
+})
+
+const welcomeChannelId = "961059103323611136"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
